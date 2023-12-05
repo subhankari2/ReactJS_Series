5ed58bd985b67b1a12477b8ad04e4aff1bbac879
subhankari2 <padhisubhankari@gmail.com>
2023-12-05 20:58:30 +0530

    JSX syntax

--- App.js
+++ App.js
@@ -1,33 +1,7 @@
-      import {ce as ce} from "react";
+      import React from "react";
       import ReactDOM from "react-dom/client";
-/**
-  <div class="header">
-    <h1>Namaste React</h1>
-    <li>About Us</li>
-    <li>Home</li>
-    <li>Support</li>
-    </div>
- */
 
-      const heading1 = ce(
-        "h1",
-        { 
-          id: "heading1" ,
-          key:'heading1'
-      },
-        "Namaste React"
-      );
-      const about = ce(
-        "li",
-        { 
-          id: "about",
-          key:'about'
-        },
-        "About Us"
-        );
-        const support=ce("li",{id:"support",key:"support"},"Suport");
-        const home=ce("li",{id:"home",key:"home"},"Home");
-        const list=ce("ul",{id:"list",key:"list"},[about,support,home]);
-      const header=ce("div",{id:"header",key:"header"},[heading1,list]);
+
+      const heading=<h1 id="heading1" key="heading1">Namaste React</h1>
       const root=ReactDOM.createRoot(document.getElementById("root"));
-      root.render(header);
+      root.render(heading);
