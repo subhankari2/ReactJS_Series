988b92f89bc00f18dee3f4b6f304d35ec50943b2
subhankari2 <padhisubhankari@gmail.com>
2023-12-05 20:15:54 +0530

    pain of using React createElement

--- App.js
+++ App.js
@@ -1,27 +1,33 @@
       import React from "react";
       import ReactDOM from "react-dom/client";
+/**
+  <div class="header">
+    <h1>Namaste React</h1>
+    <li>About Us</li>
+    <li>Home</li>
+    <li>Support</li>
+    </div>
+ */
+
       const heading1 = React.createElement(
         "h1",
         { 
           id: "heading1" ,
           key:'heading1'
       },
-        "Heading1"
+        "Namaste React"
       );
-      const heading2 = React.createElement(
-        "h1",
+      const about = React.createElement(
+        "li",
         { 
-          id: "heading2",
-          key:'heading2'
-         },
-        "Heading2"
-      );
-      const container = React.createElement("div", { id: "container" }, [
-        heading1,
-        heading2,
-      ]);
-      const root = ReactDOM.createRoot(document.getElementById("root"));
-      //react overrides root with render
-      console.log("root is created",root)
-      root.render(container);
-
+          id: "about",
+          key:'about'
+        },
+        "About Us"
+        );
+        const support=React.createElement("li",{id:"support",key:"support"},"Suport");
+        const home=React.createElement("li",{id:"home",key:"home"},"Home");
+        const list=React.createElement("ul",{id:"list",key:"list"},[about,support,home]);
+      const header=React.createElement("div",{id:"header",key:"header"},[heading1,list]);
+      const root=ReactDOM.createRoot(document.getElementById("root"));
+      root.render(header);
