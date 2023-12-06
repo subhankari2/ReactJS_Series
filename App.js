2242e39b69a23b52e63e79f1e6fdf11d8d892ede
subhankari2 <padhisubhankari@gmail.com>
2023-12-06 12:06:33 +0530

    react element and functional component creation and rendering using jsx

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
       import React from "react";
       import ReactDOM from "react-dom/client";
 
-
-      const heading=<h1 id="heading1" key="heading1">Namaste React</h1>
+      const headerElement=(
+        <h1 id="h1" key="h1">Namaste React Element</h1>
+      ) 
+      // const HeaderComponent=function(){
+      //   return (
+      //   <div>
+      //     <h1>Namaste React Functional Component</h1>
+      //     <h2>Namaste React Functional Component</h2>
+      //   </div>
+      //   )
+      // }
+      const HeaderComponent1=()=>{
+        return (
+        <div>
+          <h1>Namaste React Functional Component</h1>
+          <h2>Namaste React Functional Component</h2>
+        </div>
+        )
+      }
+      const HeaderComponent2=()=>{
+         (
+          <div>
+            <h1>Namaste React Functional Component</h1>
+            <h2>Namaste React Functional Component</h2>
+          </div>
+        )
+      }
       const root=ReactDOM.createRoot(document.getElementById("root"));
-      root.render(heading);
+      // root.render(headerElement);
+      root.render(<HeaderComponent1 />);
