a7dd9bfb47e6d32eade5eeadbce7c342dc275855
subhankari2 <padhisubhankari@gmail.com>
2023-12-06 14:26:06 +0530

    any js code inside functional component

--- App.js
+++ App.js
@@ -4,10 +4,13 @@
       const headerElement=(
         <h1 id="h1" key="h1">Namaste React Element</h1>
       ) 
+      var xyz="js code inside functional component"
       const HeaderComponent1=()=>{
         return (
         <div>
           {headerElement}
+          {console.log(xyz)}
+          {xyz}
           <h1>Namaste React Functional Component</h1>
           <h2>Namaste React Functional Component</h2>
         </div>
