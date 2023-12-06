429ac208fba241880eab3c07d4d560c5015ec988
subhankari2 <padhisubhankari@gmail.com>
2023-12-06 14:18:48 +0530

    react element inside functional component

--- App.js
+++ App.js
@@ -4,28 +4,13 @@
       const headerElement=(
         <h1 id="h1" key="h1">Namaste React Element</h1>
       ) 
-      // const HeaderComponent=function(){
-      //   return (
-      //   <div>
-      //     <h1>Namaste React Functional Component</h1>
-      //     <h2>Namaste React Functional Component</h2>
-      //   </div>
-      //   )
-      // }
       const HeaderComponent1=()=>{
         return (
         <div>
+          {headerElement}
           <h1>Namaste React Functional Component</h1>
           <h2>Namaste React Functional Component</h2>
         </div>
-        )
-      }
-      const HeaderComponent2=()=>{
-         (
-          <div>
-            <h1>Namaste React Functional Component</h1>
-            <h2>Namaste React Functional Component</h2>
-          </div>
         )
       }
       const root=ReactDOM.createRoot(document.getElementById("root"));
