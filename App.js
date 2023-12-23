bf475b65297061efdfc9663df71ac7c15d47be26
subhankari2 <padhisubhankari@gmail.com>
2023-12-23 11:26:21 +0530

    header component

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
       import React from "react";
       import ReactDOM from "react-dom/client";
 
-      const HeaderComponent=()=>{
+      const Header = () => {
         return (
-        <>
-           <div className="logo">
-            <img  src="https://cdn.dribbble.com/users/6119579/screenshots/14414735/dribble_food_court_4x.png" key="logo" height="90" width="90" alt="not found"/>
+          <header style={headerStyle}>
+            <div style={leftContainerStyle}>
+              <img src="https://cdn.dribbble.com/users/6119579/screenshots/14414735/dribble_food_court_4x.png" alt="Logo" style={logoStyle} />
             </div>
-            <div className="search">
-           <input type="text" placeholder="search"/><span><img src="https://icon-library.com/images/search-icon-png-transparent/search-icon-png-transparent-18.jpg" height="20" width="20" alt="search"/></span>
+            <div style={centerContainerStyle}>
+              <input type="text" placeholder="Search..." style={inputStyle} />
+              <span style={searchIconStyle}>&#128269;</span>
             </div>
-    <div className="user">
-      <img src="https://pluspng.com/img-png/user-png-icon-big-image-png-2240.png" height="30" width="30" alt="user"/>
-    </div>
-        </>
-        )
-      }
+            <div style={rightContainerStyle}>
+              <span style={userIconStyle}>&#128100;</span>
+            </div>
+          </header>
+        );
+      };
+      
+      // Styles
+      const headerStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '10px',
+        background: 'rgb(237 228 228)',
+        color: 'white',
+      };
+      
+      const leftContainerStyle = {
+        flex: '1',
+      };
+      
+      const logoStyle = {
+        width: '50px',
+        height: '50px',
+      };
+      
+      const centerContainerStyle = {
+        flex: '2',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      };
+      
+      const inputStyle = {
+        padding: '5px',
+        marginRight: '5px',
+        width: '200px',
+      };
+      
+      const searchIconStyle = {
+        fontSize: '20px',
+        cursor: 'pointer',
+      };
+      
+      const rightContainerStyle = {
+        flex: '1',
+        display: 'flex',
+        justifyContent: 'flex-end',
+      };
+      
+      const userIconStyle = {
+        fontSize: '20px',
+        cursor: 'pointer',
+      };
+      
+     
+
       const root=ReactDOM.createRoot(document.getElementById("root"));
-      root.render(<HeaderComponent/>);
+      root.render(<Header/>);
