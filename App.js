7ae10d44f71c2387e98c5cfecd0bfceb0d997c85
subhankari2 <padhisubhankari@gmail.com>
2023-12-24 22:31:53 +0530

    React Fragment

--- App.js
+++ App.js
@@ -3,70 +3,44 @@
 
       const Header = () => {
         return (
-          <header style={headerStyle}>
-            <div style={leftContainerStyle}>
-              <img src="https://cdn.dribbble.com/users/6119579/screenshots/14414735/dribble_food_court_4x.png" alt="Logo" style={logoStyle} />
-            </div>
-            <div style={centerContainerStyle}>
-              <input type="text" placeholder="Search..." style={inputStyle} />
-              <span style={searchIconStyle}>&#128269;</span>
-            </div>
-            <div style={rightContainerStyle}>
-              <span style={userIconStyle}>&#128100;</span>
-            </div>
-          </header>
+         <div className="header">
+         <img className="logo" alt="logo" src="https://i.pinimg.com/originals/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg" />
+         <div className="nav-items">
+            <ul >
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>Cart</li>
+            </ul>
+         </div>
+         </div>
         );
       };
-      
-      // Styles
-      const headerStyle = {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '10px',
-        background: 'rgb(237 228 228)',
-        color: 'white',
-      };
-      
-      const leftContainerStyle = {
-        flex: '1',
-      };
-      
-      const logoStyle = {
-        width: '50px',
-        height: '50px',
-      };
-      
-      const centerContainerStyle = {
-        flex: '2',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      };
-      
-      const inputStyle = {
-        padding: '5px',
-        marginRight: '5px',
-        width: '200px',
-      };
-      
-      const searchIconStyle = {
-        fontSize: '20px',
-        cursor: 'pointer',
-      };
-      
-      const rightContainerStyle = {
-        flex: '1',
-        display: 'flex',
-        justifyContent: 'flex-end',
-      };
-      
-      const userIconStyle = {
-        fontSize: '20px',
-        cursor: 'pointer',
-      };
-      
-     
+      const Body=()=>{
+        return (
+          <>
+          <div>Body component </div>
+          </>
+        )
+      }
+      const Footer = () =>{
+        return (
+          
+          <footer>copyright</footer>
+        
+        );
+      }
 
+      const AppLayout=()=>{
+        return (
+          <React.Fragment>
+          <Header/>
+          <Body/>
+          <Footer/>
+          </React.Fragment>
+
+        );
+      }
+      
       const root=ReactDOM.createRoot(document.getElementById("root"));
-      root.render(<Header/>);
+      root.render(<AppLayout/>);
