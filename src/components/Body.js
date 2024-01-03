a2fc327b9c8555d7af3d39f0d159ce79cd2514bf
subhankari2 <padhisubhankari@gmail.com>
2024-01-03 19:02:00 +0530

    search btn click showing true/false

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import {RestaurantList} from "../Constants";
 import RestaurantCard from "./RestaurantCard";
 
 const Body=()=>{
+    const [searchText,setSearchText]=useState("KFC");
+    const [searchClicked,setSearchClicked]=useState("false");
     return (
-      <div className="restaurant-list">
+        <>
+        <div className="search-container">
+            <input className="search" value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}/>
+            <button onClick={()=>{if(searchClicked==="true")setSearchClicked("false");else setSearchClicked("true")}}>search{searchClicked}</button>
+        </div>
+        <div className="restaurant-list">
         {
-          RestaurantList.map((restaurant)=>{
-            console.log(restaurant);
-           return <RestaurantCard {...restaurant.info}  key={restaurant.info.id} />
-          })
+            RestaurantList.map((restaurant)=>{
+                console.log(restaurant);
+                return <RestaurantCard {...restaurant.info}  key={restaurant.info.id} />
+            })
         }
-      </div>
+        </div>
+        </>
     )
   }
   export default Body;
