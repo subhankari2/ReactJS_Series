54531f867aeeb53a22862cf2698f86629a994850
subhankari2 <padhisubhankari@gmail.com>
2024-01-08 13:08:12 +0530

    resolving merge conflict

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-const Logo=()=>{
+ const Logo=()=>{
   return(
     <a href="/">
     <img className="logo" alt="logo" src="https://i.pinimg.com/originals/e6/17/f1/e617f1bfb9af4d9cf132cd3dec0da072.jpg" />
